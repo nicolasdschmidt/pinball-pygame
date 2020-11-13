9479d4201406c30e8b7600f3873dc76db87f83fe
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2020-11-13 16:23:10 -0300

    corrigir tetragon

--- tetragon.py
+++ tetragon.py
@@ -5,6 +5,6 @@ class Tetragon:
         self.corners = [c1, c2, c3, c4]
         self.lines = [Line(c1, c2), Line(c2, c3), Line(c3, c4), Line(c4, c1)]
 
-    def draw():
+    def draw(self):
         for line in self.lines:
             line.draw()
