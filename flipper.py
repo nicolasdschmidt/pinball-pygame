ef90201e639c0a58c0345dd6105d4a43ea1b0560
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2020-11-15 15:14:44 -0300

    corrigir física dos flippers

--- flipper.py
+++ flipper.py
@@ -1,17 +1,26 @@
-import pymunk, math
+import pymunk
+from pymunk import Vec2d
 from pygame import gfxdraw
 
 class Flipper:
-    def __init__(self, space, position, dimension, angle=0):
-        self.body = pymunk.Body(1, body_type=pymunk.Body.KINEMATIC)
+    def __init__(self, space, position, dimension, invert):
         self.width = dimension[0]
         self.height = dimension[1]
-        self.body.position = position
-        self.body.angle = math.radians(angle)
-        self.body.center_of_gravity = (self.width/2.5, self.height/2)
-        self.shape = pymunk.Poly.create_box(self.body, size=(self.width, self.height))
-        space.add(self.body,self.shape)
+        self.invert = invert
+        self.body = pymunk.Body(100, 200000)
+        self.body.position = position[0], position[1]
+        self.shape = pymunk.Poly.create_box(self.body, (self.width,self.height))
+        space.add(self.body, self.shape)
+        self.joint_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
+        self.joint_body.position = self.body.position
+        j = pymunk.PinJoint(self.body, self.joint_body, (self.invert * 30, 0), (self.invert * 30, 0))
+        s = pymunk.DampedRotarySpring(self.body, self.joint_body, 0.15, 20000000,900000)
+        space.add(j, s)
+        self.shape.group = 1
+        self.shape.elasticity = 0.4
 
+    def move(self):
+        self.body.apply_impulse_at_local_point(Vec2d.unit() * -30000, (self.invert * -50,20))
 
     def draw(self, screen):
         vertices = []
@@ -20,3 +29,4 @@ class Flipper:
             vertices.append((x,y))
         gfxdraw.aapolygon(screen, vertices, (0,255,0))
         gfxdraw.filled_polygon(screen, vertices, (0,255,0))
+        #gfxdraw.filled_circle(screen, int(self.invert * -50 + self.body.position.x), int(20 + self.body.position.y), 5, (255,255,255))
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flipper import Flipper
-import pygame, pymunk, sys, random, math
+import pygame, pymunk, sys, random
+from pymunk import pygame_util
 from pygame.locals import *
 
 from ball import Ball
 from obstacle import Obstacle
+from flipper import Flipper
 
 # inicializar biblioteca PyGame
 pygame.init()
@@ -16,6 +17,13 @@ clock = pygame.time.Clock()
 WIDTH = 400
 HEIGHT = 600
 
+# instanciar janela
+SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
+SCREEN.fill((0,0,0))
+pygame.display.set_caption("Pinball")
+
+draw_options = pygame_util.DrawOptions(SCREEN)
+
 CORNER_OFFSET = 20
 WALL_SIZE = 10
 BALL_RADIUS = 10
@@ -26,30 +34,16 @@ FLIPPER_L = 0
 FLIPPER_MAX = 45
 
 START_POS = (int(WIDTH - WALL_SIZE/2 - BALL_RADIUS), int(HEIGHT - WALL_SIZE/2 - BALL_RADIUS))
+LAUNCHER_OFFSET = WIDTH - WALL_SIZE/2 - BALL_RADIUS*2 - 10
 
 # espaço físico pymunk
 space = pymunk.Space()
 space.gravity = (0, 500)
 
 ball = Ball(space, START_POS, random.randint(750, 1200))
-#ball = Ball(space, (WIDTH/2.5, 0))
 
-
-flipper_l = Flipper(space, (WIDTH/2, HEIGHT/2), (50,10), 0)
-
-'''
-r_flipper_body = pymunk.Body(1, 100)
-r_flipper_body.position = 450, 100
-r_flipper_shape = pymunk.Poly.create_box(r_flipper_body, (450, 100))
-space.add(r_flipper_body, r_flipper_shape)
-
-r_flipper_joint_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
-r_flipper_joint_body.position = r_flipper_body.position 
-j = pymunk.PinJoint(r_flipper_body, r_flipper_joint_body, (0,0), (0,0))
-#todo: tweak values of spring better
-s = pymunk.DampedRotarySpring(r_flipper_body, r_flipper_joint_body, 0.15, 20000000,900000)
-space.add(j, s)
-'''
+flipper_l = Flipper(space, (LAUNCHER_OFFSET/3, HEIGHT*3/4), (70,10), -1)
+flipper_r = Flipper(space, (LAUNCHER_OFFSET/3*2, HEIGHT*3/4), (70,10), 1)
 
 # limites físicos da tela
 boundaries = []
@@ -59,39 +53,36 @@ boundaries.append(Obstacle(space, (WIDTH, HEIGHT/2), (10, HEIGHT))) # leste
 boundaries.append(Obstacle(space, (0, HEIGHT/2), (10, HEIGHT)))     # oeste
 boundaries.append(Obstacle(space, (WIDTH-CORNER_OFFSET, CORNER_OFFSET), (WIDTH/2, 10), 45))   # nordeste
 boundaries.append(Obstacle(space, (CORNER_OFFSET, CORNER_OFFSET), (WIDTH/2, 10), -45))   # noroeste
-boundaries.append(Obstacle(space, (WIDTH - WALL_SIZE/2 - BALL_RADIUS*2 - 10, HEIGHT/2+100), (5, HEIGHT)))
+boundaries.append(Obstacle(space, (LAUNCHER_OFFSET, HEIGHT/2+100), (5, HEIGHT)))
 #boundaries.append(Obstacle(space, (WIDTH/2+FLIPPER_OFFSET, HEIGHT/2+100), (5, HEIGHT/2)))
-
-# instanciar janela
-SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
-SCREEN.fill((0,0,0))
-pygame.display.set_caption("Pinball")
 
 # loop principal
 while True:
-    # se o usuário clicar para sair, obedecer
     for event in pygame.event.get():
+        # se o usuário clicar para sair, obedecer
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        # input do teclado
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_f:
+                flipper_l.move()
+            if event.key == pygame.K_j:
+                flipper_r.move()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(pygame.mouse.get_pos())
 
     # preencher a tela com preto (para esconder o frame anterior)
     SCREEN.fill((0,0,0))
 
     ball.draw(SCREEN)
     flipper_l.draw(SCREEN)
+    flipper_r.draw(SCREEN)
 
     for boundary in boundaries:
         boundary.draw(SCREEN)
 
-    keys=pygame.key.get_pressed()
-
-    if keys[K_z]:
-        if FLIPPER_L < FLIPPER_MAX:
-            FLIPPER_L += 10
-    else:
-        FLIPPER_L = 0
-    flipper_l.body.angle = math.radians(FLIPPER_L)
+    #space.debug_draw(draw_options)
 
     # atualizar espaço físico
     space.step(1/FPS)
